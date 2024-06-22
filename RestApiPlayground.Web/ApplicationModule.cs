15fecab80eb9d9100c1c6c150e7371df0001879d
Shashank Saini <shashanksaini203@gmail.com>
2024-06-22 20:22:56 +0530

    Web API project restructure

---
+++ RestApiPlayground.Web/ApplicationModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using RestApiPlayground.Domain.Interfaces;
+using RestApiPlayground.Infrastructure.Repositories;
+using RestApiPlayground.Service.Services;
+
+namespace RestApiPlayground.Web
+{
+    public class ApplicationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EmployeeRepository>().As<IEmployeeRepository>().SingleInstance();
+
+            builder.RegisterType<EmployeeService>().As<IEmployeeService>().SingleInstance();
+        }
+    }
+}
---
+++ RestApiPlayground.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using RestApiPlayground.Domain.Contracts;
+using RestApiPlayground.Service.Services;
+
+namespace RestApiPlayground.Web.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Employee> GetById(int id)
+        {
+            return await _employeeService.GetByIdAsync(id);
+        }
+
+        [HttpGet("getAllEmployees")]
+        public async Task<IEnumerable<Employee>> GetAll()
+        {
+            return await _employeeService.GetAllAsync();
+        }
+
+        [HttpPost("createEmployee")]
+        public async Task<IActionResult> Post([FromBody] Employee employee)
+        {
+            if (employee == null)
+            {
+                return BadRequest();
+            }
+
+            await _employeeService.AddAsync(employee);
+            return Ok();
+        }
+    }
+}
