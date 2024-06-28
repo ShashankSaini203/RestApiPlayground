d921306371a3d0973c5ad9f7aa910456ab466d7a
Shashank Saini <shashanksaini203@gmail.com>
2024-06-28 20:24:21 +0530

    Contract update

---
+++ RestApiPlayground.Domain/Contract/Base/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestApiPlayground.Domain.Contract.Base
+{
+    public class BaseEntity
+    {
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public BaseEntity()
+        {
+            this.ModifiedDate = DateTime.Now;
+        }
+    }
+}
---
+++ RestApiPlayground.Domain/Contract/Employee.cs
@@ -0,0 +1,39 @@
+﻿using RestApiPlayground.Domain.Contract.Base;
+using RestApiPlayground.Domain.Helpers.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestApiPlayground.Domain.Contracts
+{
+    public class Employee : BaseEntity
+    {
+        [Required(ErrorMessage = "FirstName is required.")]
+        [MinLength(2, ErrorMessage = "FirstName must be at least 2 characters long.")]
+        [NotPlaceholder("string", ErrorMessage = "FirstName cannot be 'string'.")]
+        public string FirstName { get; set; }
+
+
+        [MinLength(2, ErrorMessage = "LastName must be at least 2 characters long.")]
+        [NotPlaceholder("string", ErrorMessage = "LastName cannot be 'string'.")]
+        public string LastName { get; set; }
+
+
+        [MinLength(2, ErrorMessage = "Department must be at least 2 characters long.")]
+        [NotPlaceholder("string", ErrorMessage = "Department cannot be 'string'.")]
+        public string Department { get; set; }
+
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Contact Number is required.")]
+        [Phone]
+        public string ContactNumber { get; set; }
+
+
+        [MinLength(2, ErrorMessage = "Address must be at least 2 characters long.")]
+        [NotPlaceholder("string", ErrorMessage = "Department cannot be 'string'.")]
+        public string Address { get; set; }
+    }
+}
