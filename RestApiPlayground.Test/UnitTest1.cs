9216175279781343da92a474c2742ce4ed536962
Shashank Saini <shashanksaini203@gmail.com>
2024-07-04 11:09:03 +0530

    Test project addition

---
+++ RestApiPlayground.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RestApiPlayground.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
