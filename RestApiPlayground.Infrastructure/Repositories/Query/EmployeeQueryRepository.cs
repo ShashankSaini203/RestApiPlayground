9b137a2113411994c4fcd0d7094fb81f78c4b39c
Shashank Saini <shashanksaini203@gmail.com>
2024-07-05 11:26:28 +0530

    Classes fixed

--- RestApiPlayground.Infrastructure/Repositories/Query/EmployeeQueryRepository.cs
+++ RestApiPlayground.Infrastructure/Repositories/Query/EmployeeQueryRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RestApiPlayground.Infrastructure.Repositories.Query
+﻿namespace RestApiPlayground.Infrastructure.Repositories.Query
 {
     public class EmployeeQueryRepository
     {
