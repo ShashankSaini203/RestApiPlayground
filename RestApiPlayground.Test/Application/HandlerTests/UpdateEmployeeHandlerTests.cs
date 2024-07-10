5b40d08ed8030aa4c1b101edb1dba7c1d31a8b5b
Shashank Saini <shashanksaini203@gmail.com>
2024-07-10 19:55:06 +0530

    Update test assert

--- RestApiPlayground.Test/Application/HandlerTests/UpdateEmployeeHandlerTests.cs
+++ RestApiPlayground.Test/Application/HandlerTests/UpdateEmployeeHandlerTests.cs
@@ -1,6 +1,8 @@
 ﻿using Moq;
 using NUnit.Framework;
+using RestApiPlayground.Application.Commands;
 using RestApiPlayground.Application.Handlers.CommandHandler;
+using RestApiPlayground.Domain.Contracts;
 using RestApiPlayground.Domain.Repositories.Command;
 using RestApiPlayground.Infrastructure.Repositories.Command;
 
@@ -24,7 +26,22 @@ namespace RestApiPlayground.Test.Application.HandlerTests
         public async Task UpdateEmployeeHandler_ValidCommand_ShouldUpdateAndReturnUpdatedEmployee()
         {
             //Arrange
+            var updateCommand = new UpdateEmployeeCommand()
+            {
+                Id = 11,
+                FirstName = "Monkey D",
+                LastName = "Luffy",
+                Department = "Pirates",
+                Address = "Grand Line",
+                Email = "luffy@strawhats.com",
+                ContactNumber = "9878978977"
+            };
+
+            _mockCommandRepository.Setup(repo => repo.UpdateAsync(It.IsAny<Employee>())).ReturnsAsync((Employee emp) => emp);
+
             //Act
+
+
             //Assert
 
         }
