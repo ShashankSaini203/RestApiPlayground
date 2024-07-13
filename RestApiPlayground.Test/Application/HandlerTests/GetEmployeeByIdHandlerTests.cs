3b63160c8e0b3c8d4c7d21044bb1728fadc47ed4
Shashank Saini <shashanksaini203@gmail.com>
2024-07-13 23:56:33 +0530

    GetEmployeeByIdQuery Handler

--- RestApiPlayground.Test/Application/HandlerTests/GetEmployeeByIdHandlerTests.cs
+++ RestApiPlayground.Test/Application/HandlerTests/GetEmployeeByIdHandlerTests.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using NUnit.Framework;
+using RestApiPlayground.Application.Handlers.QueryHandler;
+using RestApiPlayground.Domain.Repositories.Query;
 
 namespace RestApiPlayground.Test.Application.HandlerTests
 {
-    internal class GetEmployeeByIdHandlerTests
+    [TestFixture]
+    public class GetEmployeeByIdHandlerTests
     {
+        private Mock<IEmployeeQueryRepository> _mockQueryRepository;
+        private GetEmployeeByIdHandler getEmployeeByIdHandler;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockQueryRepository = new Mock<IEmployeeQueryRepository>();
+            getEmployeeByIdHandler = new GetEmployeeByIdHandler(_mockQueryRepository.Object);
+        }
+
+        [Test]
+        public async Task GetEmployeeByIdQueryHandler_ValidQuery_ShouldReturnEmployee()
+        {
+        }
     }
 }
