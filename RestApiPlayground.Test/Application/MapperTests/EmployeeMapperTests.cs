cbd8d6bf0e152885ab5358f06eef46a1e3aa7ed9
Shashank Saini <shashanksaini203@gmail.com>
2024-07-17 20:14:40 +0530

    UpdateEmployeeCommand Mapper Test

--- RestApiPlayground.Test/Application/MapperTests/EmployeeMapperTests.cs
+++ RestApiPlayground.Test/Application/MapperTests/EmployeeMapperTests.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using RestApiPlayground.Application.Commands;
+using RestApiPlayground.Application.Mappers;
+using RestApiPlayground.Application.Responses;
+using RestApiPlayground.Domain.Contracts;
 
 namespace RestApiPlayground.Test.Application.MapperTests
 {
-    internal class EmployeeMapperTests
+    [TestFixture]
+    public class EmployeeMapperTests
     {
+        [Test]
+        public void ShouldMap_UpdateEmployeeCommand_To_Employee()
+        {
+            var updateEmployeeCommand = new UpdateEmployeeCommand()
+            {
+                Id = 1,
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "TestAddress",
+                Department = "TestDepartment",
+                ContactNumber = "TestContactNumber",
+                Email = "TestEmail"
+            };
+
+            var result = EmployeeMapper.Mapper.Map<Employee>(updateEmployeeCommand);
+
+            Assert.NotNull(result);
+            Assert.That(result.Id, Is.EqualTo(updateEmployeeCommand.Id));
+            Assert.That(result.FirstName, Is.EqualTo(updateEmployeeCommand.FirstName));
+            Assert.That(result.LastName, Is.EqualTo(updateEmployeeCommand.LastName));
+            Assert.That(result.Address, Is.EqualTo(updateEmployeeCommand.Address));
+            Assert.That(result.Department, Is.EqualTo(updateEmployeeCommand.Department));
+            Assert.That(result.ContactNumber, Is.EqualTo(updateEmployeeCommand.ContactNumber));
+            Assert.That(result.Email, Is.EqualTo(updateEmployeeCommand.Email));
+        }
     }
 }
