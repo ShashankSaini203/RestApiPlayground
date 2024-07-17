efa49c9b39fa1362c5754a0299baa7b055ceb5d6
Shashank Saini <shashanksaini203@gmail.com>
2024-07-17 20:27:18 +0530

    Mapper tests added

--- RestApiPlayground.Test/Application/MapperTests/EmployeeMapperTests.cs
+++ RestApiPlayground.Test/Application/MapperTests/EmployeeMapperTests.cs
@@ -10,6 +10,32 @@ namespace RestApiPlayground.Test.Application.MapperTests
     public class EmployeeMapperTests
     {
         [Test]
+        public void ShouldMap_CreateEmployeeCommand_To_Employee()
+        {
+            var createEmployeeCommand = new CreateEmployeeCommand()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "TestAddress",
+                Department = "TestDepartment",
+                ContactNumber = "TestContactNumber",
+                Email = "TestEmail",
+                CreationDate = DateTime.Now
+            };
+
+            var result = EmployeeMapper.Mapper.Map<Employee>(createEmployeeCommand);
+
+            Assert.NotNull(result);
+            Assert.That(result.FirstName, Is.EqualTo(createEmployeeCommand.FirstName));
+            Assert.That(result.LastName, Is.EqualTo(createEmployeeCommand.LastName));
+            Assert.That(result.Address, Is.EqualTo(createEmployeeCommand.Address));
+            Assert.That(result.Department, Is.EqualTo(createEmployeeCommand.Department));
+            Assert.That(result.ContactNumber, Is.EqualTo(createEmployeeCommand.ContactNumber));
+            Assert.That(result.Email, Is.EqualTo(createEmployeeCommand.Email));
+            Assert.That(result.CreationDate, Is.EqualTo(createEmployeeCommand.CreationDate));
+        }
+
+        [Test]
         public void ShouldMap_UpdateEmployeeCommand_To_Employee()
         {
             var updateEmployeeCommand = new UpdateEmployeeCommand()
@@ -34,5 +60,65 @@ namespace RestApiPlayground.Test.Application.MapperTests
             Assert.That(result.ContactNumber, Is.EqualTo(updateEmployeeCommand.ContactNumber));
             Assert.That(result.Email, Is.EqualTo(updateEmployeeCommand.Email));
         }
+
+        [Test]
+        public void ShouldMap_EmployeeResponse_To_Employee()
+        {
+            var updateEmployeeCommand = new EmployeeResponse()
+            {
+                Id = 1,
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "TestAddress",
+                Department = "TestDepartment",
+                ContactNumber = "TestContactNumber",
+                Email = "TestEmail"
+            };
+
+            var result = EmployeeMapper.Mapper.Map<Employee>(updateEmployeeCommand);
+
+            Assert.NotNull(result);
+            Assert.That(result.Id, Is.EqualTo(updateEmployeeCommand.Id));
+            Assert.That(result.FirstName, Is.EqualTo(updateEmployeeCommand.FirstName));
+            Assert.That(result.LastName, Is.EqualTo(updateEmployeeCommand.LastName));
+            Assert.That(result.Address, Is.EqualTo(updateEmployeeCommand.Address));
+            Assert.That(result.Department, Is.EqualTo(updateEmployeeCommand.Department));
+            Assert.That(result.ContactNumber, Is.EqualTo(updateEmployeeCommand.ContactNumber));
+            Assert.That(result.Email, Is.EqualTo(updateEmployeeCommand.Email));
+        }
+
+        [Test]
+        public void ShouldMap_Employee_To_EmployeeResponse()
+        {
+            var employee = createTestEmployee();
+
+            var result = EmployeeMapper.Mapper.Map<EmployeeResponse>(employee);
+
+            Assert.NotNull(result);
+            Assert.That(result.Id, Is.EqualTo(employee.Id.ToString()));
+            Assert.That(result.FirstName, Is.EqualTo(employee.FirstName));
+            Assert.That(result.LastName, Is.EqualTo(employee.LastName));
+            Assert.That(result.Address, Is.EqualTo(employee.Address));
+            Assert.That(result.Department, Is.EqualTo(employee.Department));
+            Assert.That(result.ContactNumber, Is.EqualTo(employee.ContactNumber));
+            Assert.That(result.Email, Is.EqualTo(employee.Email));
+            Assert.That(result.CreationDate, Is.EqualTo(employee.CreationDate));
+            Assert.That(result.ModifiedDate, Is.EqualTo(employee.ModifiedDate));
+
+        }
+
+        public Employee createTestEmployee() => new Employee()
+        {
+            Id = 1,
+            FirstName = "TestFirstName",
+            LastName = "TestLastName",
+            Address = "TestAddress",
+            Department = "TestDepartment",
+            ContactNumber = "TestContactNumber",
+            Email = "TestEmail",
+            CreationDate = DateTime.Now,
+            ModifiedDate = DateTime.Now
+
+        };
     }
 }
